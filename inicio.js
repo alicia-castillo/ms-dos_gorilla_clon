65df9d9cdc4c5cc8b2940a403ad16660d21606b0
Gio <legio8@hotmail.com>
2019-04-02 17:17:14 -0600

    Canvas
    Se agrego canvas con estilos para que la pagina no este sola.
    Instalar Jquery (npm install jquery)

--- inicio.js
+++ inicio.js
@@ -1,5 +1,7 @@
-document.addEventListener();
-
-var registroPrincipal = {
-    fps = 0 
-}
+$(document).ready(function() {
+    this.canvas = document.getElementById('canvas');
+    this.width = this.canvas.width;
+    this.height = this.canvas.height;
+    this.context = this.canvas.getContext('2d');
+  }
+)
