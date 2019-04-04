bc0b791121742e20466a91a910a7176ca654375f
alicia-castillo <alicia-castilloo@outlook.com>
2019-04-04 00:44:08 -0600

    Dibuja soÃl

--- inicio.js
+++ inicio.js
@@ -3,5 +3,23 @@ $(document).ready(function() {
     this.width = this.canvas.width;
     this.height = this.canvas.height;
     this.context = this.canvas.getContext('2d');
+    drawSun(this.context);
   }
 )
+
+function drawSun(context) {
+  // draw the colored region
+  context.beginPath();
+  context.arc(300, 35, 20, 0, 2 * Math.PI, true);
+  context.fillStyle = "#E2FFC6";
+  context.fill();
+
+  // draw the stroke
+  context.lineWidth = 20;
+  context.strokeStyle = "#66CC01";
+  context.stroke();
+}
+
+function drawScenario(){
+  
+}
