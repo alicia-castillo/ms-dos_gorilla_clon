140da30ea3055c0e8efb2b10d225a5a551d70392
Gio <legio8@hotmail.com>
2019-04-06 11:02:52 -0600

    Obtencion valores
    Se obtienen los valores que ingresan los jugadores

--- inicio.js
+++ inicio.js
@@ -2,6 +2,8 @@ $(document).ready(function() {
     document.getElementById("jugadores").style.display = "none";
     document.getElementById("alerta").style.display = "none";
     document.getElementById("nombre_j1").focus();
+    var nomb2 = document.getElementById("nombre_j2");
+    nomb2.addEventListener("keypress",tecla);
   }
 )
 
@@ -11,15 +13,19 @@ function inicia()
   this.width = this.canvas.width;
   this.height = this.canvas.height;
   this.context = this.canvas.getContext('2d');
-  var nomb_j1 = $("#nombre_j1").val();
-  var nomb_j2 = $("#nombre_j2").val();
-  if(nomb_j1 != "" && nomb_j2 != "")
+  this.nomb_j1 = $("#nombre_j1").val();
+  this.nomb_j2 = $("#nombre_j2").val();
+  if(this.nomb_j1 != "" && this.nomb_j2 != "")
   {
     document.getElementById("lab_j1").innerHTML = nomb_j1;
     document.getElementById("lab_j2").innerHTML = nomb_j2;
     document.getElementById("jugadores").style.display = "block";
     document.getElementById("inicio").style.display = "none";
     document.getElementById("alerta").style.display = "none";
+    this.tur_j1 = document.getElementById("velocidad_j1");
+    this.tur_j1.addEventListener("keypress",turnoJ1);
+    this.tur_j2 = document.getElementById("velocidad_j2");
+    this.tur_j2.addEventListener("keypress",turnoJ2);
     drawSun(this.context);
   }
   else
@@ -28,6 +34,47 @@ function inicia()
   }
 }
 
+function turnoJ1(e)
+{
+  if (e.keyCode === 13) { // 13 is enter
+    var angulo_j1 = $("#angulo_j1").val();
+    var velocidad_j1 = $("#velocidad_j1").val();
+    if(angulo_j1 != "" && velocidad_j1 != ""){
+      alert("Angulo: "+angulo_j1+", Velocidad J1:"+velocidad_j1);
+      $("#angulo_j1").val("");
+      $("#velocidad_j1").val("");
+    }
+    else{
+      alert("No dejar campos vacios");
+    }
+  }
+}
+
+function turnoJ2(e)
+{
+  if (e.keyCode === 13) { // 13 is enter
+    var angulo_j2 = $("#angulo_j2").val();
+    var velocidad_j2 = $("#velocidad_j2").val();
+    if(angulo_j2 != "" && velocidad_j2 != ""){
+      alert("Angulo J2:"+angulo_j2+", Velocidad J2:"+velocidad_j2);
+      $("#angulo_j2").val("");
+      $("#velocidad_j2").val("");
+    }
+    else
+    {
+      alert("No dejar campos vacios");
+    }
+  }
+}
+
+function tecla(e)
+{
+  var tecla = e.keyCode;
+  if (tecla === 13) { // 13 is enter
+    inicia();
+  }
+
+}
 
 function drawSun(context) {
   // draw the colored region
