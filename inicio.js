1c8e8a95890ac8f085558578ee1953de23999805
Gio <legio8@hotmail.com>
2019-04-09 21:14:15 -0500

    Personajes e intento de gameloop
    Se agregaron los personajes (fijos) y se hizo un intento de gameloop para el lanzamiento del proyectil.

--- inicio.js
+++ inicio.js
@@ -1,6 +1,26 @@
+var puntaje1 = 0;
+var puntaje2 = 0;
+var posx1 = 10;
+var posy1 = 122;
+var posx2 = 400;
+var posy2 = 180;
+
+var posPx;
+var posPy;
+
+var anchoCan;
+var altoCan;
+var contexto;
+var canvas;
+
+var turno=false;
+
 $(document).ready(function() {
     document.getElementById("jugadores").style.display = "none";
     document.getElementById("alerta").style.display = "none";
+    document.getElementById("conejo").style.display = "none";
+    document.getElementById("conejo2").style.display = "none";
+    document.getElementById("puntaje").style.display = "none";
     document.getElementById("nombre_j1").focus();
     var nomb2 = document.getElementById("nombre_j2");
     nomb2.addEventListener("keypress",tecla);
@@ -11,19 +31,22 @@ $(document).ready(function() {
 
 function inicia()
 {
-  this.canvas = document.getElementById('canvas');
-  this.width = this.canvas.width;
-  this.height = this.canvas.height;
-  this.context = this.canvas.getContext('2d');
+  canvas = document.getElementById('canvas');
+  anchoCan = canvas.width;
+  altoCan = canvas.height;
+  contexto = canvas.getContext('2d');
   this.nomb_j1 = $("#nombre_j1").val();
   this.nomb_j2 = $("#nombre_j2").val();
   if(this.nomb_j1 != "" && this.nomb_j2 != "")
   {
     document.getElementById("lab_j1").innerHTML = nomb_j1;
     document.getElementById("lab_j2").innerHTML = nomb_j2;
+    document.getElementById("puntaje").innerHTML = "  Puntajes< " + nomb_j1 + ": " + puntaje1 + " - " + puntaje2 + " :" + nomb_j2 + " >  ";
     document.getElementById("jugadores").style.display = "block";
+    document.getElementById("puntaje").style.display = "block";
     document.getElementById("inicio").style.display = "none";
     document.getElementById("alerta").style.display = "none";
+    escondej2();
     document.getElementById("angulo_j1").focus();
     this.tur_j1 = document.getElementById("velocidad_j1");
     this.tur_j1.addEventListener("keypress",turnoJ1);
@@ -34,8 +57,9 @@ function inicia()
     this.turn_j1.addEventListener("keypress",turnoJ1);
     this.turn_j2 = document.getElementById("angulo_j2");
     this.turn_j2.addEventListener("keypress",turnoJ2);
-    drawSun(this.context);
-    drawScenario(this.context);
+    window.setInterval(loopJuego(),1000/60);
+    //drawSun();
+    //drawScenario();
   }
   else
   {
@@ -43,16 +67,54 @@ function inicia()
   }
 }
 
+function escondej2(){
+  document.getElementById("angulo_j2").style.display = "none";
+  document.getElementById("velocidad_j2").style.display = "none";
+  document.getElementById("labang2").style.display = "none";
+  document.getElementById("labvel2").style.display = "none";
+}
+
+function escondej1(){
+  document.getElementById("angulo_j1").style.display = "none";
+  document.getElementById("velocidad_j1").style.display = "none";
+  document.getElementById("labang1").style.display = "none";
+  document.getElementById("labvel1").style.display = "none";
+}
+
+function muestraj2(){
+  document.getElementById("angulo_j2").style.display = "block";
+  document.getElementById("velocidad_j2").style.display = "block";
+  document.getElementById("labang2").style.display = "block";
+  document.getElementById("labvel2").style.display = "block";
+  document.getElementById("angulo_j2").focus();
+}
+
+function muestraj1(){
+  document.getElementById("angulo_j1").style.display = "block";
+  document.getElementById("velocidad_j1").style.display = "block";
+  document.getElementById("labang1").style.display = "block";
+  document.getElementById("labvel1").style.display = "block";
+  document.getElementById("angulo_j1").focus();
+}
+
 function turnoJ1(e)
 {
-  if (e.keyCode === 13) { // 13 is enter
+  if (e.keyCode === 13) { //Tecla Enter
     var angulo_j1 = $("#angulo_j1").val();
     var velocidad_j1 = $("#velocidad_j1").val();
     if(angulo_j1 != "" && velocidad_j1 != "" && !isNaN(angulo_j1)  && !isNaN(velocidad_j1)){
       alert("Angulo: "+angulo_j1+", Velocidad J1:"+velocidad_j1);
-      document.getElementById("angulo_j2").focus();
+      /*posPx = posx1+5;
+      posPy = posy1;
+      turno=true;
+      while(posPx<anchoCan){
+        posPx++;
+      }
+      turno=false;*/
       $("#angulo_j1").val("");
       $("#velocidad_j1").val("");
+      muestraj2();
+      escondej1();
     }
     else{
       alert("Introduzca un numero");
@@ -60,16 +122,28 @@ function turnoJ1(e)
   }
 }
 
+function loopJuego()
+{
+  drawSun();
+  drawScenario();
+  dibujaProyectil();
+}
+
 function turnoJ2(e)
 {
-  if (e.keyCode === 13) { // 13 is enter
+  if (e.keyCode === 13) { //Tecla Enter
     var angulo_j2 = $("#angulo_j2").val();
     var velocidad_j2 = $("#velocidad_j2").val();
     if(angulo_j2 != "" && velocidad_j2 != "" && !isNaN(angulo_j2)  && !isNaN(velocidad_j2)){
       alert("Angulo J2:"+angulo_j2+", Velocidad J2:"+velocidad_j2);
-      document.getElementById("angulo_j1").focus();
+      //posPx = posx2+5;
+      //posPy = posy2;
       $("#angulo_j2").val("");
       $("#velocidad_j2").val("");
+      muestraj1();
+      escondej2();
+      /*puntaje2 = puntaje2+1;
+      actualizaPunt(puntaje1,puntaje2);*/
     }
     else
     {
@@ -77,6 +151,12 @@ function turnoJ2(e)
     }
   }
 }
+
+function actualizaPunt(punt1,punt2)
+{
+  document.getElementById("puntaje").innerHTML = "Puntajes< " + nomb_j1 + ": " + punt1 + " - " + punt2 + " :" + nomb_j2 + " >  ";
+}
+
 
 function tecla(e)
 {
@@ -87,44 +167,61 @@ function tecla(e)
 
 }
 
-function drawSun(context) {
+function drawSun() {
   // draw the colored region
-  context.beginPath();
-  context.arc(300, 35, 20, 0, 2 * Math.PI, true);
-  context.fillStyle = "#E2FFC6";
-  context.fill();
+  contexto.beginPath();
+  contexto.arc(300, 35, 20, 0, 2 * Math.PI, true);
+  contexto.fillStyle = "#E2FFC6";
+  contexto.fill();
 
   // draw the stroke
-  context.lineWidth = 20;
-  context.strokeStyle = "#66CC01";
-  context.stroke();
+  contexto.lineWidth = 20;
+  contexto.strokeStyle = "#66CC01";
+  contexto.stroke();
 }
 
-function drawScenario(context){
-  context.beginPath();
-  context.arc(80, 420, 250, 1*Math.PI,2*Math.PI);
-  context.fillStyle = "green";
-  context.fill();
+function drawScenario(){
+  canvas.width = canvas.width;
+  contexto.beginPath();
+  contexto.arc(80, 420, 250, 1*Math.PI,2*Math.PI);
+  contexto.fillStyle = "green";
+  contexto.fill();
   // draw the stroke
-  context.lineWidth = 2;
-  context.strokeStyle = "black";
-  context.stroke();
+  contexto.lineWidth = 2;
+  contexto.strokeStyle = "black";
+  contexto.stroke();
 
-  context.beginPath();
-  context.arc(400, 420, 200, 1*Math.PI,2*Math.PI);
-  context.fillStyle = "green";
-  context.fill();
+  contexto.beginPath();
+  contexto.arc(400, 420, 200, 1*Math.PI,2*Math.PI);
+  contexto.fillStyle = "green";
+  contexto.fill();
   // draw the stroke
-  context.lineWidth = 2;
-  context.strokeStyle = "black";
-  context.stroke();
+  contexto.lineWidth = 2;
+  contexto.strokeStyle = "black";
+  contexto.stroke();
 
-  context.beginPath();
-  context.arc(700, 420, 250, 1*Math.PI,2*Math.PI);
-  context.fillStyle = "green";
-  context.fill();
+  contexto.beginPath();
+  contexto.arc(700, 420, 250, 1*Math.PI,2*Math.PI);
+  contexto.fillStyle = "green";
+  contexto.fill();
   // draw the stroke
-  context.lineWidth = 2;
-  context.strokeStyle = "black";
-  context.stroke();
+  contexto.lineWidth = 2;
+  contexto.strokeStyle = "black";
+  contexto.stroke();
+  
+  var img1 = document.getElementById("conejo");
+  contexto.drawImage(img1, posx1, posy1,100,50);
+
+  var img2 = document.getElementById("conejo2");
+  contexto.drawImage(img2, posx2, posy2,100,50);
+}
+
+function dibujaProyectil()
+{
+  if(turno){
+    contexto.beginPath();
+    contexto.arc(posPx, posPy, 5, 0, 2 * Math.PI, true);
+    contexto.fillStyle = "#E2FFC6";
+    contexto.fill();
+  }
 }
